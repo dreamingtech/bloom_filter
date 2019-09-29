d46c6cd73290fc04373def72ec0f11f117fa2adf
DreaMingTech <26395688+dreamingtech@users.noreply.github.com>
2019-09-29 15:56:17 +0800

    modify params and arguments

--- redis_bloom_filter.py
+++ redis_bloom_filter.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 # 基于 redis 的布隆过滤器
+# redis based bloom filter
 
 import math
-import six
-
 import redis
 import mmh3
 
@@ -39,75 +38,74 @@ class RedisConn(object):
 
 class BloomFilterRedis(object):
     """ redis 版布隆过滤器 """
-    def __init__(self, redis_config, data_size_per_key=10 ** 7, memory_size=500, hash_seeds_num=8, error_rate_threshold=1e-4):
+    def __init__(self, redis_db_config, redis_key_config, bf_config):
         """
-        redis 版布隆过滤器不需要初始化 bitmap/bitarray, 只需要用 setbit 把 redis 的 key 中某个 index 处的值置为 1 即可
-        通过设置的 memory_size, hash_seeds_num, 计算出能够达到的误判率和存储量
-        n = -(m * (ln2)^2)/ln(p)
-        :param redis_config: redis 的配置信息
-        :param data_size_per_key: 期望向每个 key 中保存的数据量
-        :param memory_size: redis 中每个 key 使用的内存量, 这里默认为 500
-        :param hash_seeds_num: hash 种子的数量
-        :param error_rate_threshold: 误判率的最大值
+        初始化布隆过滤器
+        - bf_config 中包含的内容
+          - data_size_per_key: 期望向每个 key 中保存的数据量
+          - memory_size: redis 中每个 key 使用的内存量
+          - hash_seeds_num: hash 种子的数量
+          - error_rate_threshold: 误判率的最大值
+
+        :param redis_db_config: redis 数据库的配置信息
+        :param redis_key_config: redis 中保存 bloom filter 内容的 key
+        :param bf_config: bloom filter 配置信息
         """
-        if not (isinstance(data_size_per_key, int) and data_size_per_key > 0):
-            raise ValueError("data_size must be greater than 0")
-        if not (0 < error_rate_threshold < 1):
+        # 要保存到 redis 每个 key 中的数据量的大小/要使用每个 redis_key 进行过滤的数据量的大小
+        self.data_size_per_key = bf_config.get("data_size_per_key")
+        # bloom 过滤器 使用的内存量
+        memory_size = bf_config.get("memory_size")
+        # hash 种子数量
+        self.hash_seeds_num = bf_config.get("hash_seeds_num")
+        # 误判率阈值, 保证给定的 data_size, hash_num, bit_size 计算得到的 error_rate 小于给定的 error_rate_threshold
+        self.error_rate_threshold = bf_config.get("error_rate_threshold")
+
+        if not (isinstance(self.data_size_per_key, int) and self.data_size_per_key > 0):
+            raise ValueError("data_size_per_key must be greater than 0")
+        if not (0 < self.error_rate_threshold < 1):
             raise ValueError("error_rate_threshold must be between 0 and 1")
         if not (isinstance(memory_size, int) and 0 < memory_size < 512):
             raise Exception('memory_size for redis must be integer and between (0 512MB)')
+        if not (isinstance(self.hash_seeds_num, int) and 0 < self.hash_seeds_num < 10):
+            raise Exception('hash_seeds_num for redis must be integer and between (0 10)')
 
-        # 要保存到 redis 每个 key 中的数据量的大小
-        self.data_size_per_key = data_size_per_key
-        # memory_size 即一个 redis_key 对应的 bit 位的长度
+        # 计算 memory_size 对应的 即一个 redis_key 对应的 bit 位的长度
         self.bit_num = memory_size * 1024 * 1024 * 8
-        # hash 种子数量
-        self.hash_seeds_num = hash_seeds_num
-        # 误判率阈值, 保证给定的 data_size, hash_num, bit_num 计算得到的 error_rate 小于给定的 error_rate_threshold
-        self.error_rate_threshold = error_rate_threshold
-
         # 计算误判率
         self.error_rate = self._cal_error_rate()
         # 检查给定的参数计算得到的误判率 error_rate 能否小于误判率阈值 error_rate_threshold
         self._check_error_rate()
 
-        # 计算一个 memory_size 能够保存的 data_size
-        self.max_data_size = self._cal_max_data_size()
-
         # 实例化 redis 客户端
-        self.redis_cli = RedisConn(redis_config).get_redis_cli()
+        self.redis_cli = RedisConn(redis_db_config).get_redis_cli()
 
         # redis 中布隆过滤器的 key, 因为可能会使用多个 内存块, 故可能有多个 redis_key
         # 把 redis_config 中定义的 bloom_filter 作为基础, 在后面添加数字作为 bloom_filter_key
         # 如 bloom_filter_01, bloom_filter_02
-        self.redis_filter_key_base = redis_config.get('bloom_filter_key', 'bloom_filter')
+        self.redis_filter_key_base = redis_key_config.get('bloom_filter_key', 'bloom_filter')
         # redis 中记录 bloom_filter 中保存数据量的 key
-        self.redis_count_key = redis_config.get('redis_count_key', 'bloom_filter_count')
+        self.redis_count_key = redis_key_config.get('redis_count_key', 'bloom_filter_count')
         # redis 分布式锁, 以免出现同时判断, 同时添加的情况
-        self.redis_lock_key = redis_config.get('redis_lock_key', 'bloom_filter_lock')
+        self.redis_lock_key = redis_key_config.get('redis_lock_key', 'bloom_filter_lock')
 
-        # 以列表的形式保存所有的 redis_filter_key, 如 [bloom_filter_1, bloom_filter_2], 用来进行去重判断
-        # 从 redis 中读取出所有 匹配 bloom_filter_0 - bloom_filter_9 的 key
-        # 如果没有获取到, 表示之前没有运行过此布隆过滤器, 就把 _filter_list 就设置为 ['bloom_filter_1']
-        self._filter_list = [_i.decode() for _i in self.redis_cli.keys(pattern='bloom_filter_[0-9]')] if \
-            self.redis_cli.keys(pattern='bloom_filter_[0-9]') else ['bloom_filter_1']
+        self._filter_list = None
+        self.data_saved = None
+        # 获取 _filter_list 和 data_saved 参数
+        self._get_init_params()
 
-        # 从 redis 中读取到的 keys 可能是乱序排列的, 要对其进行升序排列
-        self._filter_list.sort()
-
-        # 初始化时从 redis 中读取出已经保存的数据量, 如果未读取到数据, 就设置为 0
-        # 从 redis 中获取 _data_saved, 每次向 redis add 后都把 redis 中的这个键的值加 1
-        self.data_saved = int(self.redis_cli.get(self.redis_count_key).decode('utf-8')) if \
-            self.redis_cli.get(self.redis_count_key) else 0
+        # 计算一个 memory_size 能够保存的 data_size
+        self.max_data_size = self._cal_max_data_size()
 
         # 获取多个 hash 种子
         self._hash_seeds_list = self.get_hash_seeds()
 
     def _cal_error_rate(self):
         """
-        通过传入的数据量 data_size (n), 内存量 bit_num (m), hash 种子数量 hash_seeds_num (k), 计算出能够达到的 误判率 (p)
-        n 为数据量          p 为误报率
-        k 为哈希函数个数    m 为 bit 位长度
+        通过传入的数据量 data_size (n), 内存量 bit_size (m), hash 种子数量 hash_seeds_num (k), 计算出能够达到的 误判率 (p)
+        n 为数据量
+        p 为误报率
+        k 为哈希函数个数
+        m 为 bit 位长度
         n = ceil(m / (-k / log(1 - exp(log(p) / k))))
         p = pow(1 - exp(-k / (m / n)), k)
         m = ceil((n * log(p)) / log(1 / pow(2, log(2))))
@@ -132,17 +130,42 @@ class BloomFilterRedis(object):
                 'please add hash_num or increase memory_size.'.format(self.error_rate, self.error_rate_threshold)
             )
 
+    def _get_init_params(self):
+        """
+        从 redis 中读取出所有的 redis_key, 保存到列表中, 如果不存在, 就设置为 ['bf_url_1'] 或 ['bf_title_1']
+        从 redis 中读取出已经保存的 数据量, 如果不存在, 就设置为 0
+        """
+        # 以列表的形式保存所有的 redis_filter_key, 如 [bf_url_1, bf_url_2], 用来进行去重判断
+        # 如果是 url 去重, redis_key 的形式为 [bf_url_1, bf_url_2]
+        if self.redis_filter_key_base.startswith('bf_url'):
+            # 从 redis 中读取出所有 匹配 bf_url_0 - bf_url_9 的 key
+            # 如果没有获取到, 表示之前没有运行过此布隆过滤器, 就把 _filter_list 就设置为 ['bf_url_1']
+            self._filter_list = [_i.decode() for _i in self.redis_cli.keys(pattern='bf_url_[0-9]')] if \
+                self.redis_cli.keys(pattern='bf_url_[0-9]') else ['bf_url_1']
+        # 如果是 title 去重, redis_key 的形式为 [bf_title_1, bf_title_2]
+        elif self.redis_filter_key_base.startswith('bf_title'):
+            self._filter_list = [_i.decode() for _i in self.redis_cli.keys(pattern='bf_title_[0-9]')] if \
+                self.redis_cli.keys(pattern='bf_title_[0-9]') else ['bf_title_1']
+
+        # 从 redis 中读取到的 keys 可能是乱序排列的, 要对其进行升序排列
+        self._filter_list.sort()
+
+        # 初始化时从 redis 中读取出已经保存的数据量, 如果未读取到数据, 就设置为 0
+        # 从 redis 中获取 _data_saved, 每次向 redis add 后都把 redis 中的这个键的值加 1
+        self.data_saved = int(self.redis_cli.get(self.redis_count_key).decode('utf-8')) if \
+            self.redis_cli.get(self.redis_count_key) else 0
+
     def _cal_max_data_size(self):
         """
         计算在给定的 p, m, k 时, 一个 filter 能够保存的最大数据量 n
-        既然已经有了 data_size_per_filter, 为什么还要再计算 max_data_size 呢
-        data_size_per_filter 是每一个过滤器中 *期望/想要* 存入的数据量,
-        用它来保证 *实际* 存入数据时的误判率 小于 *期望* 的误判率阈值,
-        是实例化 bloom_filter 时的第一层保证, 也可以说是从 *理论* 上进行的保证
-        而 max_data_size 则是每一个过滤器在 *期望的* 误判率阈值 下, 能够 *实际* 能存入的数据量,
-        当存入的数据量大于 max_data_size 时, 却依然可以继续存入数据, 只是此时的 *实际* 误判率, 就要大于设定的误判率阈值了,
-        计算 max_data_size, 当存入的数据量 data_saved 大于等于 max_data_size 时, 就新建 过滤器来进行过滤.
-        这样, 就从 *实际* 上保证了 bloom_filter 整体上的误判率小于给定的 误判率阈值了
+        既然已经有了 data_size_per_filter, 为什么还要再计算 max_data_size 呢?
+        1. data_size_per_filter 是每一个过滤器中 *期望/想要* 使用 bf 过滤的数据量,
+        用它来保证 *实际* 过滤数据时的误判率 小于 *期望* 的误判率阈值,
+        是实例化 bloom_filter 时的第一层保证, 也可以说是从 *理论* 上进行的保证.
+        2. 而 max_data_size 则是每一个过滤器在 *期望的* 误判率阈值 下, 能够 *实际* 存入/过滤的数据量,
+        当存入/过滤 的数据量大于 max_data_size 时, 却依然可以继续存入数据, 只是此时的 *实际* 误判率,
+        就要大于设定的误判率阈值了, 计算 max_data_size, 当存入的数据量 data_saved 大于等于 max_data_size 时,
+        就新建 过滤器来进行过滤. 这样, 就从 *实际* 上保证了 bloom_filter 整体上的误判率小于给定的 误判率阈值了
         n = ceil(m / (-k / log(1 - exp(log(p) / k))))
         """
         k = self.hash_seeds_num
@@ -171,7 +194,9 @@ class BloomFilterRedis(object):
         return _seeds
 
     def get_hash_indexes(self, data):
-        """计算一个给定的数据 data 使用所有的 hash_seeds_list 得到的在 bitarray 中的索引值"""
+        """
+        计算一个给定的数据 data 使用所有的 hash_seeds_list 得到的在 bitarray 中的索引值
+        """
         # 把 str 数据转换为 bytes
         data = self._safe_data(data)
         _hash_indexes = [mmh3.hash(data, self._hash_seeds_list[_i]) % self.bit_num for _i in range(self.hash_seeds_num)]
@@ -190,14 +215,16 @@ class BloomFilterRedis(object):
 
     def add(self, data):
         """
+        redis 版布隆过滤器不需要初始化 bitmap/bitarray, 只需要用 setbit 把 redis 的 key 中某个 index 处的值置为 1 即可
         随着数据量的增加, 如果 redis 中保存的数据量 _data_saved 大于 指定的最大数据量,
         就动态增加一个内存块, 同时, 数据全部保存到新的内存块中, 但判断时, 还是从所有的内存块中进行判断
         """
 
-        # 在每次向 bloom_filter 中添加数据时, 都先判断一下数据量,
-        # 如果数据量达到所有保存的最大值, 就增加一个过滤器
+        # 在每次向 bloom_filter 中添加数据时, 都先判断一下 已经保存的数据量,
+        # 如果 已保存数据量 达到 每个 filter 所能保存的最大值, 就增加一个新的 filter
         self._check_and_add_new_filter()
 
+        # get_hash_indexes 获取所有 hash 种子对应的 hash 索引值 / offset 值
         for _hash_index in self.get_hash_indexes(data):
             self.redis_cli.setbit(self._filter_list[-1], _hash_index, 1)
 
@@ -206,7 +233,12 @@ class BloomFilterRedis(object):
         self.redis_cli.incr(self.redis_count_key)
 
     def _is_exists_in_certain_filter(self, data, _filter):
-        """ 检查给定的值 data 是否存在于某个特定的 _filter 中 """
+        """
+        检查给定的值 data 是否存在于某个特定的 _filter 中
+        对每一个 hash 种子计算 给定的 data 的 index 值, 或称索引值, 偏移量,
+        只要有一个 hash 种子计算得到的 index 不在 _filter 中, 就说明这个 data 绝对不存在
+        如果所有 hash 种子计算得到的 index 在 _filter 中都存在, 在一定的误判率下, 就认为这个 data 存在
+        """
         # 获取所有 hash 种子计算得到的 索引值
         hash_indexes = self.get_hash_indexes(data)
         # 对所有的索引值进行过滤
@@ -245,90 +277,186 @@ class BloomFilterRedis(object):
 
 def main_multi_filter():
     """
-    测试生成多个 bitarray filter
-    注意: 因为 布隆过滤器不会自动生成, 所以测试时需要手动删除 redis 中的所有相关 key
+    测试生成多个 filter
+    注意: 因为 布隆过滤器不会自动删除之前的 redis_key, 所以测试时需要手动删除 redis 中的所有相关 key
     """
-    redis_config = dict(
+    # redis 数据库配置信息
+    redis_db_config = dict(
         host='127.0.0.1',
         port=6379,
         db=0,
-        bloom_filter_key='bloom_filter',
-        redis_count_key='bloom_filter_count',
-        redis_lock_key='bloom_filter_lock',
         password=None,
     )
-    # 以下面的参数, 每个 filter 中可以保存 42 个数据
-    # 测试时需要把 data_size_per_key 设置的尽可能的小, 否则, 计算出来的误判率很可能会超过阈值
-    bf = BloomFilterRedis(
-        redis_config,
-        data_size_per_key=10 ** 1,
-        memory_size=1,
-        hash_seeds_num=2,
-        error_rate_threshold=1e-10
+
+    # url 去重的 redis key 配置信息
+    redis_key_config_url = dict(
+        bloom_filter_key='bf_url',
+        redis_count_key='bf_count_url',
+        redis_lock_key='bf_lock_url',
     )
 
-    print('params used can reach a error_rate of <{}>'.format(bf.error_rate))
-    print('params used can save <{}> data in one filter'.format(bf.max_data_size))
+    # 以下面的参数, 每个 filter 中可以保存 42 个数据
+    # 测试多个 filter 时需要把 data_size_per_key 设置的尽可能的小, 否则, 计算出来的误判率很可能会超过阈值
+    # 布隆过滤器 参数 配置信息
+    bf_config_url = dict(
+        data_size_per_key=10 ** 1,    # 期望使用 bf 去重的数据量
+        memory_size=1,                # 使用的 内存量
+        hash_seeds_num=2,             # 使用的 hash seeds 的数量
+        error_rate_threshold=1e-10,   # 误判率阈值
+    )
+    bf_url = BloomFilterRedis(
+        redis_db_config,
+        redis_key_config_url,
+        bf_config_url,
+    )
+
+    print('params used can reach a error_rate of <{}>'.format(bf_url.error_rate))
+    print('params used can save <{}> data in one filter'.format(bf_url.max_data_size))
 
     for i in range(50):
-        if bf.exists(i):
+        if bf_url.exists(i):
             print('data: <{}> exist'.format(i))
             continue
-        bf.add(i)
-        print('data: <{}> added, data_size: <{}>'.format(i, bf.data_saved))
+        bf_url.add(i)
+        print('data: <{}> added, data_size: <{}>'.format(i, bf_url.data_saved))
 
     for i in range(100):
-        if bf.exists(i):
+        if bf_url.exists(i):
             print('data: <{}> exist'.format(i))
             continue
-        bf.add(i)
-        print('data: <{}> added, data_size: <{}>'.format(i, bf.data_saved))
+        bf_url.add(i)
+        print('data: <{}> added, data_size: <{}>'.format(i, bf_url.data_saved))
+
+    print("{} {} {}".format("*" * 25, "title filter 测试", "*"*25))
+
+    # title 去重的 redis key 配置信息
+    redis_key_config_title = dict(
+        bloom_filter_key='bf_title',
+        redis_count_key='bf_count_title',
+        redis_lock_key='bf_lock_title',
+    )
+
+    # 以下面的参数, 每个 filter 中可以保存 42 个数据
+    # 测试多个 filter 时需要把 data_size_per_key 设置的尽可能的小, 否则, 计算出来的误判率很可能会超过阈值
+    # 布隆过滤器 参数 配置信息
+    bf_config_title = dict(
+        data_size_per_key=10 ** 1,    # 期望使用 bf 去重的数据量
+        memory_size=1,                # 使用的 内存量
+        hash_seeds_num=2,             # 使用的 hash seeds 的数量
+        error_rate_threshold=1e-10,   # 误判率阈值
+    )
+    bf_title = BloomFilterRedis(
+        redis_db_config,
+        redis_key_config_title,
+        bf_config_title,
+    )
+
+    print('params used can reach a error_rate of <{}>'.format(bf_title.error_rate))
+    print('params used can save <{}> data in one filter'.format(bf_title.max_data_size))
+
+    for i in range(50):
+        if bf_title.exists(i):
+            print('data: <{}> exist'.format(i))
+            continue
+        bf_title.add(i)
+        print('data: <{}> added, data_size: <{}>'.format(i, bf_title.data_saved))
+
+    for i in range(100):
+        if bf_title.exists(i):
+            print('data: <{}> exist'.format(i))
+            continue
+        bf_title.add(i)
+        print('data: <{}> added, data_size: <{}>'.format(i, bf_title.data_saved))
 
 
 def main_scrapy_single_node():
-    redis_config = dict(
+    # redis 数据库配置信息
+    redis_db_config = dict(
         host='127.0.0.1',
         port=6379,
         db=0,
-        bloom_filter_key='bloom_filter',
-        redis_count_key='bloom_filter_count',
-        redis_lock_key='bloom_filter_lock',
         password=None,
     )
-    # 对 1.4 亿条数据进行去重, 使用 500M 内存, 误判率 十万分之一
-    # n = 141834908
-    # p = 0.00001 (1 in 100000)
-    # m = 4192000000 (499.73MiB)
-    # k = 8
-    bf = BloomFilterRedis(
-        redis_config,
-        data_size_per_key=14 * 10 ** 7,
-        memory_size=500,
-        hash_seeds_num=8,
-        error_rate_threshold=1e-5
+
+    # url 去重的 redis key 配置信息
+    redis_key_config_url = dict(
+        bloom_filter_key='bf_url',
+        redis_count_key='bf_count_url',
+        redis_lock_key='bf_lock_url',
     )
 
-    print('params used can reach a error_rate of <{}>'.format(bf.error_rate))
-    print('params used can save <{}> data in one filter'.format(bf.max_data_size))
+    # 布隆过滤器 参数 配置信息
+    bf_config_url = dict(
+        data_size_per_key=14 * 10 ** 7,   # 期望使用 bf 去重的数据量
+        memory_size=500,                  # 使用的 内存量
+        hash_seeds_num=8,                 # 使用的 hash seeds 的数量
+        error_rate_threshold=1e-5,        # 误判率阈值
+    )
+    # 实例化 url 去重的 布隆过滤器 bf_url
+    bf_url = BloomFilterRedis(
+        redis_db_config,
+        redis_key_config_url,
+        bf_config_url,
+    )
+
+    print('params used can reach a error_rate of <{}>'.format(bf_url.error_rate))
+    print('params used can save <{}> data in one filter'.format(bf_url.max_data_size))
 
     words = ['when', 'how', 'where', 'too', 'there', 'to', 'when', 'a', 'b', 'c', 'd', 'e']
 
     for i in words:
         i = str(i)
-        if bf.exists(i):
+        if bf_url.exists(i):
             print('data: <{}> exist'.format(i))
             continue
-        bf.add(i)
-        print('data: <{}> added, data_size: <{}>'.format(i, bf.data_saved))
+        bf_url.add(i)
+        print('data: <{}> added, data_size: <{}>'.format(i, bf_url.data_saved))
 
     # test len, and in
-    print('data_size of bloom_filter: <{}>'.format(len(bf)))
-    print('is xixi in bloom_filter: ', 'xixi' in bf)
-    print('is where in bloom_filter: ', 'where' in bf)
+    print('data_size of bloom_filter: <{}>'.format(len(bf_url)))
+    print('is xixi in bloom_filter: ', 'xixi' in bf_url)
+    print('is where in bloom_filter: ', 'where' in bf_url)
+
+    print("{} {} {}".format("*" * 25, "title filter 测试", "*" * 25))
+
+    # title 去重的 redis key 配置信息
+    redis_key_config_title = dict(
+        bloom_filter_key='bf_title',
+        redis_count_key='bf_count_title',
+        redis_lock_key='bf_lock_title',
+    )
+
+    # 布隆过滤器 参数 配置信息
+    bf_config_title = bf_config_url
+
+    # 实例化 标题 去重的 布隆过滤器
+    bf_title = BloomFilterRedis(
+        redis_db_config,
+        redis_key_config_title,
+        bf_config_title,
+    )
+
+    print('params used can reach a error_rate of <{}>'.format(bf_title.error_rate))
+    print('params used can save <{}> data in one filter'.format(bf_title.max_data_size))
+
+    words = ['when', 'how', 'where', 'too', 'there', 'to', 'when', 'a', 'b', 'c', 'd', 'e']
+
+    for i in words:
+        i = str(i)
+        if bf_title.exists(i):
+            print('data: <{}> exist'.format(i))
+            continue
+        bf_title.add(i)
+        print('data: <{}> added, data_size: <{}>'.format(i, bf_title.data_saved))
+
+    # test len, and in
+    print('data_size of bloom_filter: <{}>'.format(len(bf_title)))
+    print('is xixi in bloom_filter: ', 'xixi' in bf_title)
+    print('is where in bloom_filter: ', 'where' in bf_title)
 
 
 if __name__ == '__main__':
-    # main_multi_filter()
-    main_scrapy_single_node()
+    main_multi_filter()
+    # main_scrapy_single_node()
 
 
--- scrapy_test.py
+++ scrapy_test.py
@@ -6,39 +6,51 @@ from redis_bloom_filter import BloomFilterRedis
 # p = 0.00001 (1 in 100000)
 # m = 480000000 (57.22MiB)
 # k = 5
-bf_memory = BloomFilterMemory(
+bf_config_memory = dict(
     data_size_per_filter=10 ** 7,
     memory_size=100,
     hash_seeds_num=5,
     error_rate_threshold=1e-6
 )
 
-redis_config = dict(
+bf_memory = BloomFilterMemory(bf_config_memory)
+
+# redis 数据库配置信息
+redis_db_config = dict(
     host='127.0.0.1',
     port=6379,
     db=0,
-    bloom_filter_key='bloom_filter',
-    redis_count_key='bloom_filter_count',
-    redis_lock_key='bloom_filter_lock',
     password=None,
 )
 
+# url 去重的 redis key 配置信息
+redis_key_config_url = dict(
+    bloom_filter_key='bf_url',
+    redis_count_key='bf_count_url',
+    redis_lock_key='bf_lock_url',
+)
+
+# 布隆过滤器 参数 配置信息
 # 对 1.4 亿条数据进行去重, 使用 500M 内存, 误判率 十万分之一
 # n = 141834908
 # p = 0.00001 (1 in 100000)
 # m = 4192000000 (499.73MiB)
 # k = 8
-bf_redis = BloomFilterRedis(
-    redis_config,
-    data_size_per_key=14 * 10 ** 7,
-    memory_size=500,
-    hash_seeds_num=8,
-    error_rate_threshold=1e-5
+bf_config_url = dict(
+    data_size_per_key=14 * 10 ** 7,  # 期望使用 bf 去重的数据量
+    memory_size=500,  # 使用的 内存量
+    hash_seeds_num=8,  # 使用的 hash seeds 的数量
+    error_rate_threshold=1e-5,  # 误判率阈值
+)
+# 实例化 url 去重的 布隆过滤器 bf_url
+bf_redis_url = BloomFilterRedis(
+    redis_db_config,
+    redis_key_config_url,
+    bf_config_url,
 )
 
-
 for i in range(50):
-    url = "https://www.dreamingtech.net/s/python{}".format(i)
+    url = "https://www.dreamingtech.net/s?kw=python{}".format(i)
 
     # 先使用内存型布隆过滤器进行过滤, 再使用 redis 型进行过滤
     # 如果内存型中不存在, 再使用 redis 去重, 如果 redis 中也不存在, 表示 url 不存在, 以前没有爬取过这个 url
@@ -52,12 +64,12 @@ for i in range(50):
         print('url: <{}> added to memory filter, data_size of memory filter: <{}>'.format(url, bf_memory.data_saved))
 
         # 如果 memory filter 中不存在, 再使用 redis filter 进行过滤
-        if bf_redis.exists(url):
+        if bf_redis_url.exists(url):
             print('url: <{}> exists in redis filter'.format(url))
             continue
         else:
-            bf_redis.add(url)
-            print('url: <{}> added to redis filter, data_size of redis filter: <{}>'.format(url, bf_redis.data_saved))
+            bf_redis_url.add(url)
+            print('url: <{}> added to redis filter, data_size of redis filter: <{}>'.format(url, bf_redis_url.data_saved))
 
     print("添加 url 到 scrapy 中. url: {}".format(url))
 
@@ -75,14 +87,11 @@ for i in range(100):
         print('url: <{}> added to memory filter, data_size of memory filter: <{}>'.format(url, bf_memory.data_saved))
 
         # 如果 memory filter 中不存在, 再使用 redis filter 进行过滤
-        if bf_redis.exists(url):
+        if bf_redis_url.exists(url):
             print('url: <{}> exists in redis filter'.format(url))
             continue
         else:
-            bf_redis.add(url)
-            print('url: <{}> added to redis filter, data_size of redis filter: <{}>'.format(url, bf_redis.data_saved))
+            bf_redis_url.add(url)
+            print('url: <{}> added to redis filter, data_size of redis filter: <{}>'.format(url, bf_redis_url.data_saved))
 
     print("添加 url 到 scrapy 中. url: {}".format(url))
-
-
-
